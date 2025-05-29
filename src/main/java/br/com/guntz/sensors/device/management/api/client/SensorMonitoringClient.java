5a4f69763655cd635cd56aab9899d30bcd9ef33f
guntz <rricardoguntzell@gmail.com>
2025-05-29 09:33:00 -0300

    feat: Implementando client declarativo com HTTP Interface

--- src/main/java/br/com/guntz/sensors/device/management/api/client/SensorMonitoringClient.java
+++ src/main/java/br/com/guntz/sensors/device/management/api/client/SensorMonitoringClient.java
@@ -2,13 +2,22 @@ package br.com.guntz.sensors.device.management.api.client;
 
 import br.com.guntz.sensors.device.management.api.model.SensorMonitoringOutput;
 import io.hypersistence.tsid.TSID;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PutExchange;
 
+@HttpExchange("/api/sensors/{sensorId}/monitoring")
 public interface SensorMonitoringClient {
 
-    void enableMonitoring(TSID sensorId);
+    @PutExchange("/enable")
+    void enableMonitoring(@PathVariable TSID sensorId);
 
-    void disableMonitoring(TSID sensorId);
+    @DeleteExchange("/enable")
+    void disableMonitoring(@PathVariable TSID sensorId);
 
-    SensorMonitoringOutput getDetail(TSID sensorId);
+    @GetExchange
+    SensorMonitoringOutput getDetail(@PathVariable TSID sensorId);
 
 }
